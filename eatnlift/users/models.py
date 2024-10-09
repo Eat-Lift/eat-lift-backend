36358fb4e37edb0aa581265a56c2c08275cbe866
xasyo <xasyo96@gmail.com>
2024-10-09 19:54:37 +0200

    Tasca 12: Email com a camp obligatori de l'usuari

--- eatnlift/users/models.py
+++ eatnlift/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    email = models.EmailField(blank=False, null=False, unique=True)
     first_name = None
     last_name = None
     is_active = None
