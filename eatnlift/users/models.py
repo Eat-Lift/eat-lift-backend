d60e5081b200647cd86f3534900b739c74974bbc
xasyo <xasyo96@gmail.com>
2024-10-28 19:38:12 +0100

    Tasca 8: Afegits camps al model d'usuari

--- eatnlift/users/models.py
+++ eatnlift/users/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserActivity(models.TextChoices):
+    SEDENTARY = 'Sedentarisme'
+    LIGHT_ACTIVITY = 'Activitat lleugera'
+    MODERATE_ACTIVITY = 'Activitat moderada'
+    INTENSE_ACTIVITY = 'Activitat intensa'
+    VERY_INTENSE_ACTIVITY = 'Activitat molt intensa'
+
+class UserGoal(models.TextChoices):
+    GAIN_MUSCLE = 'Guanyar múscul'
+    LOSE_FAT = 'Perdre greix'
+    MAINTAIN_WEIGHT = 'Mantenir el pes'
+
 class CustomUser(AbstractUser):
     first_name = None
     last_name = None
@@ -15,7 +27,10 @@ class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     picture = models.URLField(max_length=255, blank=True)
-    height = models.IntegerField(null=True, blank=True) 
+    height = models.IntegerField(null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    goal = models.CharField(max_length = 15, choices=UserGoal.choices, null=True)
+    activity = models.CharField(max_length = 22, choices=UserActivity.choices, null=True)
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
