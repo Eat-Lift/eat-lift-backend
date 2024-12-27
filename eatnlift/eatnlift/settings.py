c32acfaf96bb94ce012faeb38b1e1e40de0df6c9
xasyo <xasyo96@gmail.com>
2024-12-27 16:40:25 +0100

    Tasca 61: Configurades les variables d'entorn

--- eatnlift/eatnlift/settings.py
+++ eatnlift/eatnlift/settings.py
@@ -11,11 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o$rkzq3y$f14)h!_c0bl05f5&-jmtzmr7@8@ve)a28f@vlqf@4'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', "False").lower() == "true"
-DEBUG = True
+DEBUG = config('DEBUG', "False").lower() == "true"
 
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
@@ -78,8 +77,8 @@ DATABASES = {
     }
 }
 
-# database_url = config("DATABASE_URL")
-# DATABASES["default"] = dj_database_url.parse(database_url)
+database_url = config("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
