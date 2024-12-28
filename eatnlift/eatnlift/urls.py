58701b31849530fba960470f5b0c490eff083a28
xasyo <xasyo96@gmail.com>
2024-12-28 19:03:01 +0100

    Tasca 62: Afegit una url per comprovar conectivitat

--- eatnlift/eatnlift/urls.py
+++ eatnlift/eatnlift/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('nutrition/', include('nutrition.urls')),
     path('training/', include('training.urls')),
+    path('connectivity', views.connectivity)
 ]
---
+++ eatnlift/eatnlift/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def connectivity(request):
+    return HttpResponse(status=200)
